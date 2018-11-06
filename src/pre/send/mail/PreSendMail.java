0894fa2d8db72ee6a255bc35b1c09050347449a7
Ciort Cristina <elena.cristina.ciort@accenture.com>
2018-11-06 19:19:28 +0200

    [First commit]

---
+++ src/pre/send/mail/PreSendMail.java
@@ -0,0 +1,51 @@
+package pre.send.mail;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PreSendMail {
+
+	public Connection con;
+
+	// se declara con inainte de void pentru ca este folosit si in alte metode
+	/**
+	 * Connects to MySql DB
+	 */
+	public void connectDB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// importa driverul pentru baza de date pentru a putea functiona
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/app_01", "user", "pass");
+			// se conecteaza la baza de data folosind connection
+			// string(driver:TipBazaDeDate://link conectare BD(ip:port)/nume
+			// baza date
+			// process DB request/query
+			Statement stmt = con.createStatement();
+			// creaza un obiect prin care pot executa comenzi pe baza
+			ResultSet rs = stmt.executeQuery("select 1 from dual");
+			// folosesc stmt.executeQuery(insert sau update)
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+
+			rs.close();
+			stmt.close();
+			// while parcurge query declarat la resultSet*20
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList getMail(int[] IDs) {
+		connectDB();
+		ArrayList mails = new ArrayList();
+		// parsare cu for pe IDs, pentru fiecare id din lista selectez mailul
+		// din tabela candidati si ce este in resultSet pui in mails
+		return mails;
+	}
+}
